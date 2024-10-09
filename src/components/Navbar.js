a5f5e7b7b0c4a1d89c640ba4eba9549da9ea2779
sureshknxtwave <155468506+sureshknxtwave@users.noreply.github.com>
2024-10-09 10:03:36 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">
+        <h1>API Chaining</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
